a2ba8eed7a71a8922d5297b16cf594c2da672f42
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-16 22:20:17 -0400

    Counting the number of words with initials

--- function-test/__init__.py
+++ function-test/__init__.py
@@ -2,6 +2,10 @@ import logging
 import requests
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
+import csv 
+from io import StringIO
+import csv
+from io import StringIO
 
 # Azure Blob Storage settings
 STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=myfirstblob0803;AccountKey=jx4woBiN1hnSnP7p5ZhLcUTrdy1C6kKjPDFy/lbnDXipjsY37fDl0mM0f14faWZ5hpt1BZGJE26r+AStvEqTTQ==;EndpointSuffix=core.windows.net"
@@ -66,18 +70,50 @@ def generate_html_content(word):
     </html>
     """
 
+# def store_word_in_blob(word):
+#     """Store the fetched word in Azure Blob Storage."""
+#     blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
+#     blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
+    
+#     if blob_client.exists():
+#         existing_content = blob_client.download_blob().readall().decode('utf-8')
+#         new_content = existing_content + "\n" + word
+#     else:
+#         new_content = word
+    
+
+#     blob_client.upload_blob(new_content, overwrite=True)
+
+
+
+
+BLOB_NAME = 'words.csv'
+
 def store_word_in_blob(word):
-    """Store the fetched word in Azure Blob Storage."""
+    """Store the fetched word in Azure Blob Storage with CSV format."""
     blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
     blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
     
+    counters = {}
+    total_words = 0
+
     # Check if the blob exists
     if blob_client.exists():
-        # Append the new word to the existing content
         existing_content = blob_client.download_blob().readall().decode('utf-8')
-        new_content = existing_content + "\n" + word
-    else:
-        new_content = word
-    
-    # Upload the new content
-    blob_client.upload_blob(new_content, overwrite=True)
+        csv_reader = csv.reader(StringIO(existing_content))
+        for row in csv_reader:
+            letter, count = row
+            counters[letter] = int(count)
+            total_words += int(count)
+
+    first_letter = word[0].upper()  # get the first letter and convert to uppercase
+    counters[first_letter] = counters.get(first_letter, 0) + 1
+    total_words += 1
+
+    # Check if we reached the 200 words threshold
+    if total_words >= 200:
+        s = StringIO()
+        csv_writer = csv.writer(s)
+        for key, value in counters.items():
+            csv_writer.writerow([key, value])
+        blob_client.upload_blob(s.getvalue(), overwrite=True)
